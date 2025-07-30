10502b159f3449b382b754e04fa42b8ee4d09d15
FiveEastRoot <geun9265@gmail.com>
2025-07-31 03:52:04 +0900

    D7 추가

--- app.py
+++ app.py
@@ -702,8 +702,8 @@ def plot_likert_diverging(df, prefix="DQ7-E"):
             values=[table_df.index] + [table_df[c].tolist() for c in table_df.columns],
             align='center'
         )
-    table_fig.update_layout(margin=dict(t=5, b=5))
     ))
+    table_fig.update_layout(margin=dict(t=5, b=5))
     return fig, table_fig
 
 
