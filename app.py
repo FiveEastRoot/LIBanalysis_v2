347d8246ea1a5a697cb4121142b1c5b0183b508f
FiveEastRoot <geun9265@gmail.com>
2025-08-01 11:08:04 +0900

    심화 분석추가

--- app.py
+++ app.py
@@ -1186,7 +1186,7 @@ def page_segment_analysis(df):
         theta=cats_closed,
         fill=None,
         name="전체 평균",
-        line=dict(dash="dash", width=4, color = "black"),
+        line=dict(dash="dash", width=5, color = "black"),
         opacity=0.5
 
     ))
